e249a5b36393d91aada374fbee2cf5133be83124
Mike Bevil <mike_bevil@merck.com>
2016-11-10 14:23:12 -0500

    code check-in

---
+++ raspberry-pi/blink.js
@@ -0,0 +1,20 @@
+var onoff = require('onoff');
+
+var Gpio = onoff.Gpio;
+var led = new Gpio(4, out);
+var interval;
+
+interval = setInterval(function(){
+	var value = (led.readSync() + 1) %2;
+	led.write(value, function(){
+		console.log("Changed LED state to: " + value);
+	});
+}, 2000);
+
+process.on('SIGINT', function(){
+	clearInterval(interval);
+	led.writeSync(0);
+	led.unexport();
+	console.log('Goodbye');
+	process.exit();
+})
